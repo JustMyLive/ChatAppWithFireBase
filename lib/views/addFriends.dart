4c59db50a7cab488da9f989324fb739c6b0fee4c
lllx1994 <492426606@qq.com>
2020-07-16 13:30:58 +0900

    Prefect Notification Page. Function: receive or refuse request.

--- lib/views/addFriends.dart
+++ lib/views/addFriends.dart
@@ -115,3 +115,4 @@ class RequestListItem extends StatelessWidget {
   }
 }
 
+
--- lib/views/notifications.dart
+++ lib/views/notifications.dart
@@ -6,6 +6,9 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
+
+  int count_Test = 5;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +20,67 @@ class _NotificationsState extends State<Notifications> {
           ),
         ),
       ),
+      body: ListView.builder(
+          itemCount: count_Test,
+          itemBuilder: (context,index) {
+            return Requests(userName: 'aaa',);
+          },
+      )
     );
   }
 }
+
+class Requests extends StatelessWidget {
+
+  final String userName;
+
+  Requests ({@required this.userName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.topRight,
+        padding: EdgeInsets.only(top: 15, left: 15, right: 10),
+        child: Stack(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                SizedBox(width: 7,),
+                Text(userName, style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16,
+                ),),
+              ],
+            ),
+            Positioned(
+              right: 0,
+              child: IconButton(
+                icon: Icon(Icons.highlight_off),
+                onPressed: (){
+                  print ('refuse');
+                },
+              ),
+            ),
+            Positioned(
+              right: 50,
+              child: IconButton(
+                icon: Icon(Icons.person_add),
+                onPressed: (){
+                  print ('recive');
+                },
+              ),
+            ),
+          ],
+        )
+    );
+  }
+}
+
